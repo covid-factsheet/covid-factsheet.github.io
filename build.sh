1314c563542a9b099ed03012916444d5ef902323
Bryan Buecking <bryan@crunkey.com>
2020-04-02 19:11:19 -0700

    build: Update to deal w/new dir layout.

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ debug () {
     echo "$1"
 }
 
-debug "copy" && cp ~/Downloads/projectname.tar.gz .
-debug "untar" && tar -zxf projectname.tar.gz
-debug "mv extracted dir" && cp -r cloud9/cv19app/* .
-debug "cleanup" && rm -rf cloud9/ && rm projectname.tar.gz
+debug "copy" && cp ~/Downloads/debug.tar.gz .
+debug "backup" && mv ~/Downloads/debug.tar.gz \
+    "/tmp/debug.$(date +'%FT%T').tar.gz"
+
+debug "untar" && tar -zxf debug.tar.gz
+debug "mv extracted dir" && cp -r debug/* .
+debug "cleanup" && rm -rf debug/ && rm debug.tar.gz
